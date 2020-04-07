b374e98374dabc4981ffea8a21dfc370cb165c65
unknown <med.harik@gmail.com>
2020-04-07 11:11:45 +0000

    database import/ export"

--- mvc/db2020revision.sql
+++ mvc/db2020revision.sql
@@ -1,155 +1,278 @@
--- --------------------------------------------------------
--- Hôte :                        localhost
--- Version du serveur:           10.1.37-MariaDB - mariadb.org binary distribution
--- SE du serveur:                Win32
--- HeidiSQL Version:             9.5.0.5332
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mar. 07 avr. 2020 à 13:02
+-- Version du serveur :  10.1.37-MariaDB
+-- Version de PHP :  7.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `db2020revision`
+--
 
--- Listage de la structure de la base pour db2020revision
-DROP DATABASE IF EXISTS `db2020revision`;
-CREATE DATABASE IF NOT EXISTS `db2020revision` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `db2020revision`;
+-- --------------------------------------------------------
 
--- Listage de la structure de la table db2020revision. absence
-DROP TABLE IF EXISTS `absence`;
-CREATE TABLE IF NOT EXISTS `absence` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Structure de la table `absence`
+--
+
+CREATE TABLE `absence` (
+  `id` int(11) NOT NULL,
   `date_absence` varchar(100) DEFAULT NULL,
   `nombreHeure` varchar(255) DEFAULT NULL,
   `details` text,
-  `etudiant_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_100` (`etudiant_id`),
-  CONSTRAINT `fk_100` FOREIGN KEY (`etudiant_id`) REFERENCES `etudiant` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+  `etudiant_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table db2020revision.absence : ~5 rows (environ)
-DELETE FROM `absence`;
-/*!40000 ALTER TABLE `absence` DISABLE KEYS */;
+--
+-- Déchargement des données de la table `absence`
+--
+
 INSERT INTO `absence` (`id`, `date_absence`, `nombreHeure`, `details`, `etudiant_id`) VALUES
-	(26, '2020-03-12', '', NULL, 5),
-	(27, '2020-04-01', '3', NULL, 6),
-	(28, '2020-04-01', '3', NULL, 5),
-	(29, '2020-03-31', '7', NULL, 6),
-	(30, '2020-03-31', '10', NULL, 5);
-/*!40000 ALTER TABLE `absence` ENABLE KEYS */;
+(26, '2020-03-12', '', NULL, 5),
+(27, '2020-04-01', '3', NULL, 6),
+(28, '2020-04-01', '3', NULL, 5),
+(29, '2020-03-31', '7', NULL, 6),
+(30, '2020-03-31', '10', NULL, 5);
 
--- Listage de la structure de la table db2020revision. categorie
-DROP TABLE IF EXISTS `categorie`;
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
   `nom` varchar(200) NOT NULL,
-  `chemin` varchar(300) NOT NULL DEFAULT 'images/icon.png',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `chemin` varchar(300) NOT NULL DEFAULT 'images/icon.png'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table db2020revision.categorie : ~4 rows (environ)
-DELETE FROM `categorie`;
-/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
+--
+-- Déchargement des données de la table `categorie`
+--
+
 INSERT INTO `categorie` (`id`, `nom`, `chemin`) VALUES
-	(4, 'Eletro', '../images/174a1fb5c13883e1a8360432a9cb1583en_construction.jpg'),
-	(5, 'Accessoire Auto', 'images/icon.png'),
-	(6, 'Produit Cosmetique', 'images/icon.png'),
-	(7, 'Produit nettoyage', 'images/icon.png');
-/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
+(4, 'Eletro', '../images/174a1fb5c13883e1a8360432a9cb1583en_construction.jpg'),
+(5, 'Accessoire Auto', 'images/icon.png'),
+(6, 'Produit Cosmetique', 'images/icon.png'),
+(7, 'Produit nettoyage', 'images/icon.png');
 
--- Listage de la structure de la table db2020revision. classe
-DROP TABLE IF EXISTS `classe`;
-CREATE TABLE IF NOT EXISTS `classe` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 
--- Listage des données de la table db2020revision.classe : ~14 rows (environ)
-DELETE FROM `classe`;
-/*!40000 ALTER TABLE `classe` DISABLE KEYS */;
+--
+-- Structure de la table `classe`
+--
+
+CREATE TABLE `classe` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `classe`
+--
+
 INSERT INTO `classe` (`id`, `nom`) VALUES
-	(1, 'di2'),
-	(2, 'di1'),
-	(3, 'GI 1'),
-	(4, 'licence gestion '),
-	(5, 'licence inf'),
-	(6, 'harik mohamed'),
-	(7, 'harik mohamed'),
-	(8, 'harik mohamed'),
-	(9, 'harik mohamed'),
-	(10, 'harik mohamed'),
-	(11, 'harik mohamed'),
-	(12, 'harik mohamed'),
-	(13, 'savio '),
-	(14, 'savio milandou'),
-	(15, 'CESAISTE'),
-	(16, 'prevent default');
-/*!40000 ALTER TABLE `classe` ENABLE KEYS */;
+(1, 'di2'),
+(2, 'di1'),
+(3, 'GI 1'),
+(4, 'licence gestion '),
+(5, 'licence inf'),
+(6, 'harik mohamed'),
+(7, 'harik mohamed'),
+(8, 'harik mohamed'),
+(9, 'harik mohamed'),
+(10, 'harik mohamed'),
+(11, 'harik mohamed'),
+(12, 'harik mohamed'),
+(13, 'savio '),
+(14, 'savio milandou'),
+(15, 'CESAISTE'),
+(16, 'prevent default');
 
--- Listage de la structure de la table db2020revision. etudiant
-DROP TABLE IF EXISTS `etudiant`;
-CREATE TABLE IF NOT EXISTS `etudiant` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+CREATE TABLE `etudiant` (
+  `id` int(11) NOT NULL,
   `nomprenom` varchar(100) DEFAULT NULL,
   `chemin` varchar(255) DEFAULT NULL,
-  `classe_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_12` (`classe_id`),
-  CONSTRAINT `fk_12` FOREIGN KEY (`classe_id`) REFERENCES `classe` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `classe_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table db2020revision.etudiant : ~2 rows (environ)
-DELETE FROM `etudiant`;
-/*!40000 ALTER TABLE `etudiant` DISABLE KEYS */;
+--
+-- Déchargement des données de la table `etudiant`
+--
+
 INSERT INTO `etudiant` (`id`, `nomprenom`, `chemin`, `classe_id`) VALUES
-	(5, 'dassoah', 'images/7da3605cfeb3c496d12952dd013909e4elite.jpg', 1),
-	(6, 'excellence', 'images/a06802591402b4815b04ac8a301724714.jpg', 1);
-/*!40000 ALTER TABLE `etudiant` ENABLE KEYS */;
+(5, 'dassoah', 'images/7da3605cfeb3c496d12952dd013909e4elite.jpg', 1),
+(6, 'excellence', 'images/a06802591402b4815b04ac8a301724714.jpg', 1);
 
--- Listage de la structure de la table db2020revision. produit
-DROP TABLE IF EXISTS `produit`;
-CREATE TABLE IF NOT EXISTS `produit` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id` int(11) NOT NULL,
   `libelle` varchar(100) NOT NULL,
   `prix` float DEFAULT NULL,
   `qtestock` float DEFAULT NULL,
   `chemin` varchar(300) DEFAULT 'images/icon.png',
-  `categorie_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `categorie_id` (`categorie_id`),
-  CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `categorie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table db2020revision.produit : ~2 rows (environ)
-DELETE FROM `produit`;
-/*!40000 ALTER TABLE `produit` DISABLE KEYS */;
+--
+-- Déchargement des données de la table `produit`
+--
+
 INSERT INTO `produit` (`id`, `libelle`, `prix`, `qtestock`, `chemin`, `categorie_id`) VALUES
-	(5, 'noh noh', 100, 9, 'images/a132ac1345e5367d26f94bb2ca49a8e7banner4.jpg', 7);
-/*!40000 ALTER TABLE `produit` ENABLE KEYS */;
+(5, 'noh noh', 100, 9, 'images/a132ac1345e5367d26f94bb2ca49a8e7banner4.jpg', 7);
 
--- Listage de la structure de la table db2020revision. users
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `email` varchar(200) DEFAULT NULL,
   `passe` varchar(100) NOT NULL,
-  `nom` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `nom` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table db2020revision.users : ~2 rows (environ)
-DELETE FROM `users`;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+--
+-- Déchargement des données de la table `users`
+--
+
 INSERT INTO `users` (`id`, `email`, `passe`, `nom`) VALUES
-	(1, 'admin@gmail.com', '1234', 'ADMINOS'),
-	(2, 'king@gmail.com', '1234', 'kingos');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+(1, 'admin@gmail.com', '1234', 'ADMINOS'),
+(2, 'king@gmail.com', '1234', 'kingos');
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `absence`
+--
+ALTER TABLE `absence`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_100` (`etudiant_id`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `classe`
+--
+ALTER TABLE `classe`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_12` (`classe_id`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorie_id` (`categorie_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `absence`
+--
+ALTER TABLE `absence`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `classe`
+--
+ALTER TABLE `classe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `absence`
+--
+ALTER TABLE `absence`
+  ADD CONSTRAINT `fk_100` FOREIGN KEY (`etudiant_id`) REFERENCES `etudiant` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+  ADD CONSTRAINT `fk_12` FOREIGN KEY (`classe_id`) REFERENCES `classe` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
